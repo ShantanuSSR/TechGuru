eb465ffb99f7d84788c83510b609ee83c72ea8c4
ShantanuSSR <103843863+ShantanuSSR@users.noreply.github.com>
2023-01-12 10:44:34 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechGuru</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.facebook.com/shantanu.kumar.315428" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://www.instagram.com/shantanusingh710/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/ShantanuSSR" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>TechGuru</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            <div><h2>Let's Grow Together</h2></div>
+            <div>Amazing companies start with amazing people.We are creating a 
+              diverse team of smart and passionate people who feel comfortable
+              being themselves, and are motivated by our shared goal -- improving
+              people's lives with the world best technologies.
+            </div>
+
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
